caee1882ecdc20fb440c28ab1b94d6430b4a7157
Mohammed A Khalilia <mkhalilia3@mercury.cc.gatech.edu>
2014-08-21 00:01:32 -0400

    update

--- demo.py
+++ demo.py
@@ -1,11 +1,12 @@
 import MySQLdb, sys
-sys.path.append("/nethome/mkhalilia3/dev")
-from snomed.net import net, nx
+import networkx as nx
+from snomed.net import net
 
-dbconn = MySQLdb.connect(host="localhost",user="root",passwd="",db="snomed_ct")
+dbconn = MySQLdb.connect(host="localhost",user="root",passwd="C0msc29.gatech",db="snomed_ct")
 
 #Clinical finding hierarchy parent concept ID: 404684003 (starting point)
 #  testing: 73211009 (diabetes)
 #  testing: 199223000 (Diabetes mellitus during pregnancy)
 sn = net(dbconn, 199223000)
 nx.write_gml(sn.graph,"snomed_clinical_finding.gml")
+del sn
