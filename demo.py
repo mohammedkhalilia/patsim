425682c233dcb6f55dc428ab3267a622035084d5
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2014-08-22 09:28:08 -0400

    update

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
 import MySQLdb
-from snomedtools.net import net
+from snomed_tools.network import Network
 
 #create database connection
 dbconn = MySQLdb.connect(host="localhost",user="root",passwd="",db="snomed_ct")
@@ -9,13 +9,13 @@ dbconn = MySQLdb.connect(host="localhost",user="root",passwd="",db="snomed_ct")
 #  testing: 199223000 (Diabetes mellitus during pregnancy)
 
 #build snomed network starting with a specific concept id and pass the db connection
-sn = net(dbconn, 199223000)
+sn = Network(dbconn, 199223000)
 
 #network stats
 print "Number of nodes %d, number of edges %d" % (len(sn.graph.nodes()), len(sn.graph.edges()))
 
 #write the network g to a GML file
-sn.save("/tmp/snomed_clinical_finding.gml")
+sn.save("/tmp/snomed_clinical_finding_test.gml")
 
 #clean up
 del sn
